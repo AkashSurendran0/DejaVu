1ad6c2f2bacb7432fcef3ec5286f1caf15b5876c
AkashSurendran0 <akashsurendran.personal@gmail.com>
2026-06-03 22:31:12 +0530

    fix: added env google for callback url

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ const env=require('dotenv').config()
 passport.use(new googleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback',
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
     scope: ['profile','email']
 },
 
